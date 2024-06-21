baba05c207bc5092bc3d74d7dd4c5525e36efb78
tekla kvekvetsia <kvekveciatekla@gmail.com>
2024-06-21 14:35:51 +0400

    productitem js

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,13 +1,48 @@
-import {useDispatch} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux';
+
+import { cartActions } from '../../store/cart-slice';
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
-import { cartActions } from '../../store/cart-slice';
+
 const ProductItem = (props) => {
-  const { title, price, description ,id} = props;
-  const dispatch=useDispatch()
- const addToCartHandler=()=>{
-  dispatch(cartActions.addItemToCart({id, title,price}))
- }
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  const { title, price, description, id } = props;
+
+  const addToCartHandler = () => {
+    const newTotalQuantity = cart.totalQuantity + 1;
+
+    const updatedItems = cart.items.slice(); 
+    const existingItem = updatedItems.find((item) => item.id === id);
+    if (existingItem) {
+      const updatedItem = { ...existingItem }; 
+      updatedItem.quantity++;
+      updatedItem.totalPrice = updatedItem.totalPrice + price;
+      const existingItemIndex = updatedItems.findIndex(
+        (item) => item.id === id
+      );
+      updatedItems[existingItemIndex] = updatedItem;
+    } else {
+      updatedItems.push({
+        id: id,
+        price: price,
+        quantity: 1,
+        totalPrice: price,
+        name: title,
+      });
+    }
+
+    const newCart = {
+      totalQuantity: newTotalQuantity,
+      items: updatedItems,
+    };
+
+    dispatch(cartActions.replaceCart(newCart));
+
+   
+  };
+
   return (
     <li className={classes.item}>
       <Card>
